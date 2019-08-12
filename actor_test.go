ad06d0c8872675175229632a593578be4346acdd
Arceliar <Arceliar@users.noreply.github.com>
2019-08-11 20:18:58 -0500

    add another channel benchmark and sort benchmarks

--- actor_test.go
+++ actor_test.go
@@ -20,14 +20,6 @@ func TestSyncExec(t *testing.T) {
 	}
 }
 
-func BenchmarkSyncExec(b *testing.B) {
-	var a Actor
-	f := func() {}
-	for i := 0; i < b.N; i++ {
-		a.SyncExec(f)
-	}
-}
-
 func TestSendMessageTo(t *testing.T) {
 	var a Actor
 	done := make(chan struct{})
@@ -47,16 +39,12 @@ func TestSendMessageTo(t *testing.T) {
 	}
 }
 
-func BenchmarkSendMessageTo(b *testing.B) {
+func BenchmarkSyncExec(b *testing.B) {
 	var a Actor
 	f := func() {}
-	a.SyncExec(func() {
-		for i := 0; i < b.N; i++ {
-			a.SendMessageTo(&a, f)
-		}
-	})
-	// Wait for the actor to finish
-	a.SyncExec(func() {})
+	for i := 0; i < b.N; i++ {
+		a.SyncExec(f)
+	}
 }
 
 func BenchmarkBackpressure(b *testing.B) {
@@ -70,6 +58,18 @@ func BenchmarkBackpressure(b *testing.B) {
 	done := make(chan struct{})
 	a0.Enqueue(func() { a0.SendMessageTo(&a1, func() { close(done) }) })
 	<-done
+}
+
+func BenchmarkSendMessageTo(b *testing.B) {
+	var a Actor
+	f := func() {}
+	a.SyncExec(func() {
+		for i := 0; i < b.N; i++ {
+			a.SendMessageTo(&a, f)
+		}
+	})
+	// Wait for the actor to finish
+	a.SyncExec(func() {})
 }
 
 func BenchmarkEnqueue(b *testing.B) {
@@ -95,6 +95,25 @@ func BenchmarkEnqueueDelayRunning(b *testing.B) {
 	a.SyncExec(func() {})
 }
 
+func BenchmarkChannelsSyncExec(b *testing.B) {
+	ch := make(chan func())
+	done := make(chan struct{})
+	go func() {
+		for f := range ch {
+			f()
+		}
+		close(done)
+	}()
+	f := func() {}
+	for i := 0; i < b.N; i++ {
+		d := make(chan struct{})
+		ch <- func() { f(); close(d) }
+		<-d
+	}
+	close(ch)
+	<-done
+}
+
 func BenchmarkChannels(b *testing.B) {
 	ch := make(chan func())
 	done := make(chan struct{})
@@ -112,7 +131,24 @@ func BenchmarkChannels(b *testing.B) {
 	<-done
 }
 
-func BenchmarkBufferedChannels(b *testing.B) {
+func BenchmarkSmallBufferedChannels(b *testing.B) {
+	ch := make(chan func(), 1)
+	done := make(chan struct{})
+	go func() {
+		for f := range ch {
+			f()
+		}
+		close(done)
+	}()
+	f := func() {}
+	for i := 0; i < b.N; i++ {
+		ch <- f
+	}
+	close(ch)
+	<-done
+}
+
+func BenchmarkLargeBufferedChannels(b *testing.B) {
 	ch := make(chan func(), b.N)
 	done := make(chan struct{})
 	go func() {
