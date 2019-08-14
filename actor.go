b4f6fbc30095cfa62e6d004bf5b735662238d619
Arceliar <Arceliar@users.noreply.github.com>
2019-08-13 20:22:31 -0500

    use atomic pointer operations instead of mutexes

--- actor.go
+++ actor.go
@@ -4,10 +4,31 @@
 // Message passing is asynchronous with unbounded queues, but with backpressure to pause an Actor that sends to a significantly more congested one.
 package phony
 
-import "sync"
+import (
+	"sync/atomic"
+	"unsafe"
+)
 
 // How large a queue can be before backpressure slows down sending to it.
 const backpressureThreshold = 127
+
+// A message in the queue
+type queueElem struct {
+	msg  func()
+	next unsafe.Pointer
+}
+
+func loadMsg(p *unsafe.Pointer) *queueElem {
+	return (*queueElem)(atomic.LoadPointer(p))
+}
+
+func storeMsg(p *unsafe.Pointer, msg *queueElem) {
+	atomic.StorePointer(p, unsafe.Pointer(msg))
+}
+
+func casMsg(dest *unsafe.Pointer, oldMsg, newMsg *queueElem) bool {
+	return atomic.CompareAndSwapPointer(dest, unsafe.Pointer(oldMsg), unsafe.Pointer(newMsg))
+}
 
 // An Actor maintans an inbox of messages and processes them 1 at a time.
 // The intent is for the Actor struct to be embedded in other structs, where the other fields of the struct are only read or modified by the Actor.
@@ -15,8 +36,9 @@ const backpressureThreshold = 127
 // It is up to the user to ensure that memory is used safely, and that messages do not contain blocking operations.
 // An Actor must not be copied after first use.
 type Actor struct {
-	mutex sync.Mutex
-	queue []func()
+	head unsafe.Pointer //*queueElem, accessed atomically
+	tail unsafe.Pointer //*queueElem, accessed atomically
+	size int64
 }
 
 // IActor is the interface for Actors, based on their ability to enqueue and send messages.
@@ -32,15 +54,26 @@ func (a *Actor) Enqueue(f func()) int {
 	if f == nil {
 		panic("tried to send nil message")
 	}
-	a.mutex.Lock()
-	run := a.queue == nil
-	a.queue = append(a.queue, f)
-	l := len(a.queue)
-	a.mutex.Unlock()
-	if run {
-		go a.run()
+	q := &queueElem{msg: f}
+	for {
+		// No matter what, we need to enqueue it in place of the current tail
+		tail := loadMsg(&a.tail)
+		if !casMsg(&a.tail, tail, q) {
+			// It was updated in the mean time, try again
+			continue
+		}
+		// We updated the tail, now decide what to do about the old tail
+		if tail != nil {
+			//An old tail exists, so update its next pointer to reference q
+			storeMsg(&tail.next, q)
+		} else {
+			// No old tail existed, so no worker is currently running
+			// Update the head to point to q, then start the worker
+			storeMsg(&a.head, q)
+			go a.run()
+		}
+		return int(atomic.AddInt64(&a.size, 1))
 	}
-	return l
 }
 
 // SendMessageTo should only be called on an actor by itself, and sends a message to another actor.
@@ -67,18 +100,25 @@ func (a *Actor) SyncExec(f func()) {
 
 func (a *Actor) run() {
 	for {
-		var f func()
-		a.mutex.Lock()
-		if len(a.queue) > 0 {
-			f, a.queue = a.queue[0], a.queue[1:]
-		} else {
-			a.queue = nil
-		}
-		a.mutex.Unlock()
-		if f != nil {
-			f()
-		} else {
-			return
+		atomic.AddInt64(&a.size, -1)
+		head := loadMsg(&a.head)
+		head.msg()
+		for {
+			next := loadMsg(&head.next)
+			storeMsg(&a.head, next)
+			if next != nil {
+				// Nothing more to do
+				break
+			} else {
+				if !casMsg(&a.tail, head, nil) {
+					// The head is not the tail, but there was no head.next when we checked
+					// Somebody must be updating it right now, so try again
+					continue
+				} else {
+					// Head and tail are now both nil, our work here is done, exit
+					return
+				}
+			}
 		}
 	}
 }
